77dca5d89f4b0e33235bd6c4ec4e9add36e51083
Mona Lisa <subastianhambali@gmail.com>
2025-04-15 17:08:38 +0700

    menambahkan  icon

--- src/components/MoviesComponent/MoviesHeader.jsx
+++ src/components/MoviesComponent/MoviesHeader.jsx
@@ -1,8 +1,9 @@
+import { FcRight } from "react-icons/fc";
 export default function Headers({title,url}){
     return (
         <div className='header flex justify-between'>
       <h1 className="text-2xl md:text-3xl mb-6 md:mb-10 font-semibold">{title}</h1>
-      <a className="text-blue-600" href={url}>See More  </a>
+      <a className="text-blue-600 flex" href={url}>See More<FcRight className="ms-2"/>  </a>
       </div>
     )
 
