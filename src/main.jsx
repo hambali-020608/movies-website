1a02c5840bdcc9524d89f3b48924548d1e19e26c
Mona Lisa <subastianhambali@gmail.com>
2025-04-17 23:45:39 +0700

    mengganti react router dengan react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter,Routes,Route, HashRouter } from 'react-router'
+import { BrowserRouter,Routes,Route, HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './pages/App.jsx'
 import Movies from './pages/MoviesPage.jsx'
